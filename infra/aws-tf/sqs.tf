cd3d3f1c1d5946303a3ce7c2732af64ba99d174c
Eduardo Ramos <eduardoahramos@outlook.com>
2025-08-04 00:15:43 -0300

    feat: new queue

--- infra/aws-tf/sqs.tf
+++ infra/aws-tf/sqs.tf
@@ -9,3 +9,15 @@ resource "aws_sqs_queue" "credit_request_queue" {
     Environment = "prod"
   }
 }
+
+resource "aws_sqs_queue" "credit_response_queue" {
+  name                      = "credit-response-queue"
+  delay_seconds             = 90
+  max_message_size          = 1024
+  message_retention_seconds = 86400
+  receive_wait_time_seconds = 10
+
+  tags = {
+    Environment = "prod"
+  }
+}
